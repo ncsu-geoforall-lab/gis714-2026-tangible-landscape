f0a148c7e689138c9964f4e901cdf0e7a53048c2
jilliangreene <127781302+jilliangreene@users.noreply.github.com>
2026-04-06 16:31:34 -0400

    Add Jillian's Activity
    * Jillian test files
    
    * update files
    
    * add lake sim and roads
    
    * Apply suggestion from @chaedri
    
    Co-authored-by: Caitlin Haedrich <69856275+chaedri@users.noreply.github.com>
    
    * Update activities/jillian.json
    
    Co-authored-by: Anna Petrasova <kratochanna@gmail.com>
    
    * ponds
    
    * fix ponds
    
    * adjust colors
    
    * black formatting
    
    * Apply suggestion from @chaedri
    
    * Apply suggestion from @chaedri
    
    ---------
    
    Co-authored-by: Caitlin Haedrich <69856275+chaedri@users.noreply.github.com>
    Co-authored-by: Anna Petrasova <kratochanna@gmail.com>

---
+++ activities/jillian.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import os
+
+import grass.script as gs
+
+
+def run_slope(scanned_elev, env, **kwargs):
+    gs.run_command("r.slope.aspect", elevation=scanned_elev, slope="slope", env=env)
+    gs.run_command("r.colors", map="slope", color="oranges")
+
+
+def run_ponds(scanned_elev, env, **kwargs):
+    repeat = 2
+    input_dem = scanned_elev
+    output = "tmp_filldir"
+    for i in range(repeat):
+        gs.run_command(
+            "r.fill.dir", input=input_dem, output=output, direction="tmp_dir", env=env
+        )
+        input_dem = output
+    # filter depression deeper than 0.1 m to
+    gs.mapcalc(
+        "{new} = if({out} - {scan} > 0.1, {out} - {scan}, null())".format(
+            new="ponds", out=output, scan=scanned_elev
+        ),
+        env=env,
+    )
+    gs.write_command(
+        "r.colors", map="ponds", rules="-", stdin="0% aqua\n100% blue", env=env
+    )
+
+
+def run_waterflow(scanned_elev, env, **kwargs):
+    # first we need to compute x- and y-derivatives
+    gs.run_command(
+        "r.slope.aspect", elevation=scanned_elev, dx="scan_dx", dy="scan_dy", env=env
+    )
+    gs.run_command(
+        "r.sim.water",
+        elevation=scanned_elev,
+        dx="scan_dx",
+        dy="scan_dy",
+        man_value=0.4,
+        rain_value=300,
+        depth="flow",
+        env=env,
+    )
+
+
+def main():
+    env = os.environ.copy()
+    env["GRASS_OVERWRITE"] = "1"
+    elevation = "elev_lid792_1m"
+    elev_resampled = "elev_resampled"
+    gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
+    gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
+
+    # run_slope(scanned_elev=elev_resampled, env=env)
+    run_waterflow(scanned_elev=elev_resampled, env=env)
+    run_ponds(scanned_elev=elev_resampled, env=env)
+
+
+if __name__ == "__main__":
+    main()
