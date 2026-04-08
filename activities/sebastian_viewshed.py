51939aa1a1428a496c7f07e4965209c1b262c492
Juan Sebastian Nieto Lawrene <jsnietol@ncsu.edu>
2026-04-08 12:20:34 -0400

    Updating viewshed code.

--- activities/sebastian_viewshed.py
+++ activities/sebastian_viewshed.py
@@ -3,30 +3,65 @@
 import os
 import grass.script as gs
 
-def run_marker(scanned_elev, elevation, env, **kwargs):
-    gs.mapcalc("difference = $scanned_elev - $elevation", scanned_elev=scanned_elev, elevation=elevation)
-    gs.mapcalc("difference = if(difference == 1, 1, null())")
 
-def run_viewshed(elevation, difference, env, **kwargs):
-    gs.run_command("g.region", raster=difference, zoom=difference, env=env)
-    region = gs.parse_command("g.region", flags="g")
-    x_centre = (float(region["e"]) + float(region["w"])) / 2
-    y_centre = (float(region["n"]) + float(region["s"])) / 2
-    gs.run_command("g.region", raster="ortho_2001_t792_1m@PERMANENT")
-    gs.run_command("r.viewshed", input=elevation, output="viewshed", coordinates=f"{x_centre},{y_centre}", env=env)
+def run_points(scanned_elev, env, **kwargs):
+
+    points = "points"
+    import analyses
+
+    analyses.change_detection(
+        "scan_saved",
+        scanned_elev,
+        points,
+        height_threshold=[10, 100],
+        cells_threshold=[5, 50],
+        add=True,
+        max_detected=5,
+        debug=True,
+        env=env,
+    )
+
+    data = (
+        gs.read_command(
+            "v.out.ascii",
+            input=points,
+            type="point",
+            format="point",
+            separator="comma",
+            env=env,
+        )
+        .strip()
+        .splitlines()
+    )
+
+    if data:
+        x, y = [float(p) for p in data[0].split(",")[:2]]
+        return x, y
+
+
+def run_viewshed(scanned_elev, x, y, env, **kwargs):
+    gs.run_command(
+        "r.viewshed",
+        input=scanned_elev,
+        output="viewshed",
+        coordinates=f"{x},{y}",
+        env=env,
+    )
+
 
 def main():
     env = os.environ.copy()
     env["GRASS_OVERWRITE"] = "1"
-    elevation = "elevation"
-    region = "ortho_2001_t792_1m"
+    elevation = "elev_lid792_1m"
     elev_resampled = "elev_resampled"
-    difference = "difference"
-    gs.run_command("g.region", raster=region, res=4, flags="a", env=env)
+    gs.run_command("g.region", raster=elevation, res=4, flags="a", env=env)
     gs.run_command("r.resamp.stats", input=elevation, output=elev_resampled, env=env)
 
-    run_marker(scanned_elev=elev_resampled, elevation=elevation, env=env)
-    run_viewshed(elevation=elevation, difference=difference, env=env)
+    coords = run_points(scanned_elev=elev_resampled, env=env)
+    if coords is not None:
+        x, y = coords
+        run_viewshed(scanned_elev=elev_resampled, x=x, y=y, env=env)
+
 
 if __name__ == "__main__":
     main()
